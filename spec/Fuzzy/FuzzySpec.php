2d08470911e3f90f87cc6e4250587dde3bdb2724
Ilya S <public.ilya.s@gmail.com>
2014-04-15 22:28:02 +0700

    not sure if Fuzzy/Fuzzy is a good pick, probably Fuzzy/Searcher?

---
+++ spec/Fuzzy/FuzzySpec.php
@@ -0,0 +1,16 @@
+<?php namespace spec\Fuzzy;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FuzzySpec extends ObjectBehavior {
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Fuzzy\Fuzzy');
+    }
+
+
+
+}
+
---
+++ src/Fuzzy/Fuzzy.php
@@ -0,0 +1,8 @@
+<?php namespace Fuzzy;
+
+class Fuzzy {
+
+
+
+}
+
